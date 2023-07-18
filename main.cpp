9d7d62f3bded81d5a37e05319c2a274d123ee173
lilac613 <yablondani@gmail.com>
2023-07-18 17:35:16 -0400

    added method to Point class

--- main.cpp
+++ main.cpp
@@ -43,7 +43,7 @@ class Point{
       double distance = std::pow(std::pow(x,2) + std::pow(y,2),0.5);
       return distance;
     }
-    double distance_to_point(const Point p){
+    double distance_to_point(Point p){
       double distance = std::abs(std::pow(std::pow(p.get_x() - x,2) + std::pow(p.get_y() - y,2),0.5));
       return distance;
     }
@@ -52,6 +52,12 @@ class Point{
     }
     double get_y(){
       return y;
+    }
+    void change_x(double amt){
+      x+=amt;
+    }
+    void change_y(double amt){
+      y+=amt;
     }
 };
 
@@ -69,7 +75,9 @@ class Line{
     }
     double distance_to_point(Point p){
       // write this function
-      return 0.0;
+      double numerator = std::abs((p2.get_x()-p1.get_x())*(p1.get_y() - p.get_y()) - (p1.get_x() - p.get_x())*(p2.get_y()-p1.get_y()));
+      double denominator = std::pow(std::pow(p2.get_x()-p1.get_x(),2)+std::pow(p2.get_y()-p1.get_y(),2),0.5);
+      return numerator/denominator;
     }
 };
 
@@ -113,9 +121,9 @@ class Polygon{
     double perimeter(){
       double sum = 0.0;
       for(int i = 0; i < vec.size()-1; i++){
-        sum += vec[i].distance_to_point(vec[i+1];)
+        sum += vec[i].distance_to_point(vec[i+1]);
       }
-      sum += vec[0].distance_to_point(vec.size()-1);
+      sum += vec[0].distance_to_point(vec[vec.size()-1]);
       return sum;
     }
 };
@@ -128,18 +136,18 @@ class AUV{
     double heading;
     std::array<double,3> speed;
     double angular_speed;
-    void step(const double dt){
-      position.get_x() += speed[0]*dt;
-      position.get_y() += speed[1]*dt;
+    void step(double dt){
+      position.change_x(speed[0]*dt);
+      position.change_y(speed[1]*dt);
       depth += speed[2]*dt;
       heading += angular_speed*dt;
     }
-    void apply_acceleration(const std::array<double,3> lin_accel, const double dt){
+    void apply_acceleration(std::array<double,3> lin_accel, double dt){
       speed[0] += lin_accel[0]*dt;
       speed[1] += lin_accel[1]*dt;
       speed[2] += lin_accel[2]*dt;
     }
-    void apply_angular_acceleration(const double angular_acceleration, const double dt){
+    void apply_angular_acceleration(double angular_acceleration, double dt){
       angular_speed += angular_acceleration*dt;
     }
 };
