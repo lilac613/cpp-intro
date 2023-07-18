fe6978929b5651c3504aa36097f15b4acc514a85
lilac613 <yablondani@gmail.com>
2023-07-18 11:59:16 -0400

    started c++

--- main.cpp
+++ main.cpp
@@ -2,11 +2,22 @@
 
 int main()
 {
+  std::cout << "Daniela" << std::endl;
+  std:: cout << "24-115" << std::endl;
   std::cout << "Hello, world!" << std::endl;
 
   int x = 5;
   int y = 7;
-  std::cout << "The sum of " << x << " and " << y << " is " << x + y << std::endl;
+  int z = x+y;
+  std::cout << x+y << std::endl;
+  std::cout << z << std::endl;
+  int w = x*y*z;
+  std::cout << x*y*z << std::endl;
+  std::cout << w << std::endl;
+  int v = x/y;
+  std::cout << x/y << std::endl;
+  std::cout << v << std::endl;
+  
 
   return 0;
 }
